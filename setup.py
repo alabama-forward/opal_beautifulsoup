ec39cf35f3eb06693a8ce97749dd3eecd16a927a
cabancubero <cabancubero@gmail.com>
2025-06-13 12:57:27 -0500

    Update toml and setup files.

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     install_requires=[
         "requests",
         "beautifulsoup4",
+        "selenium>=4.0.0",
+        "webdriver-manager>=4.0.0",
         "pytest"
     ],
     entry_points={
@@ -15,8 +17,23 @@ setup(
         ],
     },
     author="Gabriel Cabán Cubero",
-    author_email="cabancubero@gmail.com",
-    description="OPAL - Oppositional Positions in Alabama",
-    keywords="news, parser, web-scraping",
+    author_email="gabri@alforward.org",
+    description="OPAL - Oppositional Positions in Alabama: Web scraper for Alabama news sites and court records",
+    keywords="news, parser, web-scraping, court-records, alabama",
     python_requires=">=3.6",
+    url="https://github.com/alabamaforward/opal",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
 )
